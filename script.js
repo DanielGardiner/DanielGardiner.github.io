9fa303c7097f05930e6ace58c631c359e0e78381
Daniel Gardiner <gardiner.daniel@hotmail.co.uk>
2019-10-05 19:35:28 +0100

    further attempts to make images responsive and have included hover feature for Proj2

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ $(function() {
         $('#proj1-hover').slideToggle(500)
     })
 
+    $('#proj2-item').hover(function () {
+        $('#proj2-hover').slideToggle(500)
+    })
+
+
     // $('#proj1-item').click(function () {
     //     $('#proj1-hover').slideToggle(500)
     // })
