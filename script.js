f0a0be247b2404c77e3941de369ce6bbb79d383b
Daniel Gardiner <gardiner.daniel@hotmail.co.uk>
2019-10-05 20:04:54 +0100

    added template project panels

--- script.js
+++ script.js
@@ -8,6 +8,21 @@ $(function() {
         $('#proj2-hover').slideToggle(500)
     })
 
+    $('#proj3-item').hover(function () {
+        $('#proj3-hover').slideToggle(500)
+    })
+
+    $('#proj4-item').hover(function () {
+        $('#proj4-hover').slideToggle(500)
+    })
+    $('#proj5-item').hover(function () {
+        $('#proj5-hover').slideToggle(500)
+    })
+
+    $('#proj6-item').hover(function () {
+        $('#proj6-hover').slideToggle(500)
+    })
+
 
     // $('#proj1-item').click(function () {
     //     $('#proj1-hover').slideToggle(500)
