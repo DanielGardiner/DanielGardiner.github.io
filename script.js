d25247fd3420a6a5bfe52bbae3143ee4addb4e0c
Daniel Gardiner <gardiner.daniel@hotmail.co.uk>
2019-10-27 16:44:06 +0000

    added fixed links to each project

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 $(function() {
 
-    $('.hover-container').hover(function () {
-        $('#proj1-hover').slideToggle(500)
-    })
-
-    $('#proj2-item').hover(function () {
-        $('#proj2-hover').slideToggle(500)
-    })
-
-    $('#proj3-item').hover(function () {
-        $('#proj3-hover').slideToggle(500)
-    })
-
-    $('#proj4-item').hover(function () {
-        $('#proj4-hover').slideToggle(500)
-    })
-    $('#proj5-item').hover(function () {
-        $('#proj5-hover').slideToggle(500)
-    })
-
-    $('#proj6-item').hover(function () {
-        $('#proj6-hover').slideToggle(500)
-    })
+    // $('.hover-container').hover(function () {
+    //     $('#proj1-hover').slideToggle(500)
+    // })
+    //
+    // $('#proj2-item').hover(function () {
+    //     $('#proj2-hover').slideToggle(500)
+    // })
+    //
+    // $('#proj3-item').hover(function () {
+    //     $('#proj3-hover').slideToggle(500)
+    // })
+    //
+    // $('#proj4-item').hover(function () {
+    //     $('#proj4-hover').slideToggle(500)
+    // })
+    // $('#proj5-item').hover(function () {
+    //     $('#proj5-hover').slideToggle(500)
+    // })
+    //
+    // $('#proj6-item').hover(function () {
+    //     $('#proj6-hover').slideToggle(500)
+    // })
 
 
 
