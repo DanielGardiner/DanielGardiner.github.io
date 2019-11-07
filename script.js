f5db437c4764c34a78b72c88cb62fbf606dbdae9
Daniel Gardiner <gardiner.daniel@hotmail.co.uk>
2019-11-07 21:21:09 +0000

    updated responsivness issue with swivle button

--- script.js
+++ script.js
@@ -88,12 +88,4 @@ $(function() {
         }
     })
 
-    $('.panel-collapse').on('show.bs.collapse', function () {
-        $(this).siblings('.panel-heading').addClass('active');
-    });
-
-    $('.panel-collapse').on('hide.bs.collapse', function () {
-        $(this).siblings('.panel-heading').removeClass('active');
-    });
-
 })
